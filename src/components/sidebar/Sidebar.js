ff371254a77d8725137c9c41783e562b07f8bbfd
AkashMoh <akashmohan@protonmail.com>
2020-12-29 21:18:42 +0530

    sidebar with functionality, few tweaks in design necessary

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,106 +1,112 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { React, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
 
-import Dashboard from './dashboard/Dashboard';
-import Products from './products/Products';
-import Inventory from './inventory/Inventory';
-import Purchases from './purchases/Purchases';
-import Sales from './sales/Sales';
-import Suppliers from './suppliers/Suppliers';
-import Customers from './customers/Customers';
-import Reports from './reports/Reports';
-import Settings from './settings/Settings';
+import Dashboard from '../dashboard/Dashboard';
+import Products from '../products/Products';
+import Inventory from '../inventory/Inventory';
+import Purchases from '../purchases/Purchases';
+import Sales from '../sales/Sales';
+import Suppliers from '../suppliers/Suppliers';
+import Customers from '../customers/Customers';
+import Reports from '../reports/Reports';
+import Settings from '../settings/Settings';
 
-import dashboardIcon from '../../icons/dashboard.svg';
-import analyticsIcon from '../../icons/analytics.svg';
-import customersIcon from '../../icons/customers.svg';
-import inventoryIcon from '../../icons/inventory.svg';
-import productsIcon from '../../icons/products.svg';
-import purchasesIcon from '../../icons/purchases.svg';
-import salesIcon from '../../icons/sales.svg';
-import settingsIcon from '../../icons/settings.svg';
-import suppliersIcon from '../../icons/suppliers.svg';
+import {ReactComponent as DashboardIcon} from '../../icons/dashboard.svg';
+import {ReactComponent as AnalyticsIcon} from '../../icons/analytics.svg';
+import {ReactComponent as CustomersIcon} from '../../icons/customers.svg';
+import {ReactComponent as InventoryIcon} from '../../icons/inventory.svg';
+import {ReactComponent as ProductsIcon} from '../../icons/products.svg';
+import {ReactComponent as PurchasesIcon} from '../../icons/purchases.svg';
+import {ReactComponent as SalesIcon} from '../../icons/sales.svg';
+import {ReactComponent as SettingsIcon} from '../../icons/settings.svg';
+import {ReactComponent as SuppliersIcon} from '../../icons/suppliers.svg';
+
+import {ReactComponent as ToggleArrow} from '../../icons/toggleArrow.svg';
 
 import './Sidebar.css';
 
 function Sidebar() {
+
+    const [toggle, setToggle] = useState(true);
+
     return (
         <Router>
             <div className='main__container'>
-            <div className='sidebar__container'>
-                <ul className='list__items'>
-                <div className='sidebar__top'>
-                <li>
-                    <Link className='router__links' to="/"><img src={dashboardIcon} alt="dashboard"/>Dashboard</Link>
-                </li>
-                <li>
-                    <Link className='router__links' to="/products"><img src={productsIcon} alt="products"/>Products</Link>
-                </li>
-                <li>
-                    <Link className='router__links' to="/inventory"><img src={inventoryIcon} alt="inventory"/>Inventory</Link>
-                </li>
-                <li>
-                    <Link className='router__links' to="/purchases"><img src={purchasesIcon} alt="purchases"/>Purchases</Link>
-                </li>
-                <li>
-                    <Link className='router__links' to="/sales"><img src={salesIcon} alt="sales"/>Sales</Link>
-                </li>
-                <li>
-                    <Link className='router__links' to="/suppliers"><img src={suppliersIcon} alt="suppliers"/>Suppliers</Link>
-                </li>
-                <li>
-                    <Link className='router__links' to="/customers"><img src={customersIcon} alt="customers"/>Customers</Link>
-                </li>
-                <li>
-                    <Link className='router__links' to="/analytics"><img src={analyticsIcon} alt="analytics"/>Analytics</Link>
-                </li>
+                <div className= {toggle?'sidebar__container':'sidebar__container__collapsed'}>
+                    <ul className='list__items'>
+                        <div className='sidebar__top'>
+                            
+                            <h4>Open Capsule</h4>
+
+                            <li>
+                                {/*NavLink has an active class automatically added to it, whereas Link does'nt */}
+                                <NavLink exact className='router__links' to="/"><DashboardIcon />Dashboard</NavLink> 
+                            </li>
+                            <li>
+                                <NavLink className='router__links' to="/products"><ProductsIcon />Products</NavLink>
+                            </li>
+                            <li>
+                                <NavLink className='router__links' to="/inventory"><InventoryIcon />Inventory</NavLink>
+                            </li>
+                            <li>
+                                <NavLink className='router__links' to="/purchases"><PurchasesIcon />Purchases</NavLink>
+                            </li>
+                            <li>
+                                <NavLink className='router__links' to="/sales"><SalesIcon />Sales</NavLink>
+                            </li>
+                            <li>
+                                <NavLink className='router__links' to="/suppliers"><SuppliersIcon />Suppliers</NavLink>
+                            </li>
+                            <li>
+                                <NavLink className='router__links' to="/customers"><CustomersIcon />Customers</NavLink>
+                            </li>
+                            <li>
+                                <NavLink className='router__links' to="/analytics"><AnalyticsIcon />Analytics</NavLink>
+                            </li>
+                        </div>
+                        <div>
+                            <li>
+                                <NavLink className='router__links' to="/settings"><SettingsIcon />Settings</NavLink>
+                            </li>
+                        </div>
+                    </ul>
                 </div>
-                <div>
-                <li>
-                    <Link className='router__links' to="/settings"><img src={settingsIcon} alt="settings"/>Settings</Link>
-                </li>
+
+                <div className={toggle?'right__container__collapsed':'right__container'}>
+                    <div className='navbar'>
+                        <ToggleArrow className='toggleArrow' onClick={() => setToggle(!toggle)}/>
+                        
+                    </div>
+                    <Switch>
+                        <Route exact path="/">
+                            <Dashboard />
+                        </Route>
+                        <Route path="/products">
+                            <Products />
+                        </Route>
+                        <Route path="/inventory">
+                            <Inventory />
+                        </Route>
+                        <Route path="/purchases">
+                            <Purchases />
+                        </Route>
+                        <Route path="/sales">
+                            <Sales />
+                        </Route>
+                        <Route path="/suppliers">
+                            <Suppliers />
+                        </Route>
+                        <Route path="/customers">
+                            <Customers />
+                        </Route>
+                        <Route path="/analytics">
+                            <Reports />
+                        </Route>
+                        <Route path="/settings">
+                            <Settings />
+                        </Route>
+                    </Switch>
                 </div>
-                </ul>
-            </div>
-
-            
-
-            <div className='right__container'>
-
-            <h3>Hello there</h3>
-            
-            <Switch>
-                <Route exact path="/">
-                    <Dashboard />
-                </Route>
-                <Route path="/products">
-                    <Products />
-                </Route>
-                <Route path="/inventory">
-                    <Inventory />
-                </Route>
-                <Route path="/purchases">
-                    <Purchases />
-                </Route>
-                <Route path="/sales">
-                    <Sales />
-                </Route>
-                <Route path="/suppliers">
-                    <Suppliers />
-                </Route>
-                <Route path="/customers">
-                    <Customers />
-                </Route>
-                <Route path="/analytics">
-                    <Reports />
-                </Route>
-                <Route path="/settings">
-                    <Settings />
-                </Route>
-            </Switch>
-
-            
-            </div>
         </div>
     </Router>    
   )
